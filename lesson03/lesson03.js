225ca246c2830c7c6e22be8cf45ad414a0090fea
monakkoWF <163200054+monakkoWF@users.noreply.github.com>
2024-03-20 14:01:00 +0100

    Add files via upload

---
+++ lesson03/lesson03.js
@@ -0,0 +1,22 @@
+const titleProject = '360 academy'
+console.log(typeof titleProject);
+
+const screensValue = 'С уникальным дизайном, с анимацией'
+console.log(typeof screensValue);
+
+const screenPrice = '30$'
+console.log(typeof screenPrice);
+
+const percentage = '50'
+console.log(typeof percentage);
+
+const fullPrice = '3000$'
+console.log(typeof fullPrice);
+
+const responsive = {
+    isWorked: true
+}
+console.log(typeof responsive);
+
+console.log(titleProject, screensValue, screenPrice,
+    percentage, fullPrice, responsive);
