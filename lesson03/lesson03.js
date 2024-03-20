b46a14d2adda9759b879d109fb23a53cd8690d08
monakkoWF <163200054+monakkoWF@users.noreply.github.com>
2024-03-20 14:42:19 +0100

    Update lesson03.js

--- lesson03/lesson03.js
+++ lesson03/lesson03.js
@@ -4,18 +4,16 @@ console.log(typeof titleProject);
 const screensValue = 'С уникальным дизайном, с анимацией'
 console.log(typeof screensValue);
 
-const screenPrice = '30$'
+const screenPrice = 3000
 console.log(typeof screenPrice);
 
-const percentage = '50'
+const percentage = 50
 console.log(typeof percentage);
 
-const fullPrice = '3000$'
+const fullPrice = 3000
 console.log(typeof fullPrice);
 
-const responsive = {
-    isWorked: true
-}
+const responsive = true
 console.log(typeof responsive);
 
 console.log(titleProject, screensValue, screenPrice,
