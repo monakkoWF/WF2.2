71895c6d57276ae405da1b02ff1f16a84059a3ec
monakkoWF <alexei.sevko@gmail.com>
2024-03-21 10:32:58 +0100

    updated

--- lesson04/lesson04.js
+++ lesson04/lesson04.js
@@ -4,7 +4,7 @@ console.log(titleProject);
 let screensValue = prompt('шаблонные, с уникальным дизайном, с анимацией?')
 console.log(screensValue);
 
-let screenPrice = 3000
+let screenPrice = 10000
 console.log(screenPrice);
 
 let responsive = confirm('нужен ли респонсивный сайт?')
@@ -26,37 +26,24 @@ console.log(servicePrice2);
 let fullPrice = (screenPrice + servicePrice1 + servicePrice2)
 console.log(fullPrice);
 
-let servicePercentPrice = (fullPrice-(fullPrice*0.15))
-console.log(Math.round(servicePercentPrice));
+let PartnerPrecentage = 10
+
+let partnerPrecentageValue = +(fullPrice * (PartnerPrecentage/100))
+console.log(partnerPrecentageValue, 'Процент партнеру:');
+
+let servicePercentPrice = +(fullPrice-partnerPrecentageValue)
+console.log(Math.round(servicePercentPrice), 'Чистая прибыль:');
 
 if (fullPrice > 50000) {
-    console.log ('Скидка 10%')
-}
-
-if (fullPrice > 20000 & fullPrice < 50000) {
-    console.log ('Скидка 5%')
-}
-
-if (fullPrice < 20000 & fullPrice > 0) {
-    console.log ('Cкидка не предусмотрена')
-}
-
-if (fullPrice < 0) {
-    console.log ('Что то пошло не так')
-}
-
-if (fullPrice == 0) {
-        console.log ('Cкидка не предусмотрена')
-}
-
-if (fullPrice = 0) {
-    console.log ('Cкидка не предусмотрена')
-}
-
-if (fullPrice = 20000) {
-    console.log ('Cкидка не предусмотрена')
-}
-
-if (fullPrice = 50000) {
-    console.log ('Cкидка не предусмотрена')
+    console.log ('Скидка 10%');
+} else if (fullPrice > 20000 && fullPrice < 50000) {
+    console.log ('Скидка 5%');
+} else if (fullPrice < 20000 && fullPrice > 0) {
+    console.log ('Cкидка не предусмотрена');
+} if (fullPrice < 0) {
+    console.log ('Что то пошло не так');
+} else if (fullPrice === 0 || fullPrice === 20000 || fullPrice === 50000) {
+    console.log ('Cкидка не предусмотрена');
+} else if (fullPrice === undefined) {
+    console.log ('Cкидка не предусмотрена');
 }
